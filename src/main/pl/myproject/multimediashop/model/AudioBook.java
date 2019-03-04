ef4576aa099ac7fa54aefea9162f2444657b3d8a
Daria Peziak-Kowalska <d.peziak.kowalska@gmail.com>
2019-03-04 21:16:29 +0100

    class AudioBook

---
+++ src/main/pl/myproject/multimediashop/model/AudioBook.java
@@ -0,0 +1,41 @@
+package pl.myproject.multimediashop.model;
+
+import java.util.Objects;
+
+public class AudioBook extends Book {
+
+    public AudioBook() {
+    }
+
+    private Format format;
+
+    public Format getFormat() {
+        return format;
+    }
+
+    public void setFormat(Format format) {
+        this.format = format;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        AudioBook audioBook = (AudioBook) o;
+        return format == audioBook.format;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), format);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("AudioBook{");
+        sb.append("format=").append(format);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/pl/myproject/multimediashop/model/Format.java
@@ -0,0 +1,6 @@
+package pl.myproject.multimediashop.model;
+
+public enum Format {
+
+    MP3, FLAC;
+}
